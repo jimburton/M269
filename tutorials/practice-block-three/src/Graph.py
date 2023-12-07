84fdd9bdcc24e41838308c4653bd061395a8c1ad
James Burton <jimburton1@gmail.com>
2023-12-07 15:15:23 +0000

    Practice Block Three

--- tutorials/practice-block-three/src/Graph.py
+++ tutorials/practice-block-three/src/Graph.py
@@ -19,7 +19,7 @@ class Graph:
         """
         Convenience method for constructing a graph from sets of nodes and
         edges. Nodes may be values of any type. Edges must be tuples,
-        otherwise a RuntimeError is thrown.
+        otherwise a LookupError is thrown.
         """
         g = Graph()
         g.nodes = ns
@@ -27,7 +27,7 @@ class Graph:
             if isinstance(e, tuple):
                 g.add_edge(e)
             else:
-                raise RuntimeError(f"Not a tuple of nodes: {e}")
+                raise LookupError(f"Not a tuple of nodes: {e}")
         return g
 
     def add_node(self, n) -> None:
@@ -40,10 +40,10 @@ class Graph:
     def add_edge(self, e) -> None:
         """
         Adds an edge to the graph. Inconsistencies in the edges (eg either
-        element of the tuple is not in the graph) will cause a RuntimeError.
+        element of the tuple is not in the graph) will cause a LookupError.
         """
         if e[0] not in self.nodes or e[1] not in self.nodes:
-            raise RuntimeError(f"One or both nodes not in graph: {e}")
+            raise LookupError(f"One or both nodes not in graph: {e}")
         if e not in self.edges:
             self.edges.add(e)
 
@@ -69,7 +69,7 @@ class Graph:
         The set of nodes that are connected to n by an edge, where n is the
         source of that edge.
 
-        Throws a RuntimeError if n is not in the graph. 
+        Throws a LookupError if n is not in the graph. 
         """
         pass
 
@@ -78,7 +78,7 @@ class Graph:
         The set of nodes that are connected to n by an edge, where n is the
         target of that edge.
 
-        Throws a RuntimeError if n is not in the graph. 
+        Throws a LookupError if n is not in the graph. 
         """
         pass
 
@@ -87,7 +87,7 @@ class Graph:
         Performs a recursive depth-first traversal of the graph starting at
         n, and returns the node labels into a list.
 
-        Throws a RuntimeError if n is not in the graph. 
+        Throws a LookupError if n is not in the graph. 
         """
         pass
 
@@ -97,7 +97,7 @@ class Graph:
         n, and returns the node labels into a list. Here we are using a plain
         old list as a stack.
 
-        Throws a RuntimeError if n is not in the graph.
+        Throws a LookupError if n is not in the graph.
         """
         pass
         
@@ -107,7 +107,7 @@ class Graph:
         n, and returns the node labels into a list. Here we are using a plain old
         list as a queue.
 
-        Throws a RuntimeError if n is not in the graph.
+        Throws a LookupError if n is not in the graph.
         """
         pass
 
@@ -118,16 +118,3 @@ class Graph:
         return f"({self.nodes}, {self.edges})"
 
 # End of the Graph class
-    
-# a couple of trees for playing in the REPL
-                    
-tree0 = Graph.build({'A', 'B', 'C', 'D'},     # nodes
-                    {('A', 'B'), ('A', 'C')}) # edges
-
-tree1 = Graph.build({'A', 'B', 'C', 'D', 'E', 'F', 'G'}     # nodes
-                    , {('A', 'B'), ('A', 'C'), ('A', 'E')   # edges
-                       , ('B', 'D'), ('B', 'F'), ('C', 'G')
-                       , ('E', 'F')})
-
-tree_discon = Graph.build({'A', 'B', 'C', 'D', 'E', 'F', 'G'}     # nodes
-                    , {('A', 'C'), ('A', 'F')})
