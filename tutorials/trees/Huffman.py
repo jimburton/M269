51059f5f273bec31619440948f007ee027b30454
James Burton <jimburton1@gmail.com>
2024-02-12 10:54:03 +0000

    Removed Huffman solution.

--- tutorials/trees/Huffman.py
+++ tutorials/trees/Huffman.py
@@ -3,79 +3,26 @@ from Tree import *
 
 def freq_table(input: str) -> dict:
     """Build a frequency table from the input string."""
-    ft = dict()
-    for c in input:
-        ft[c] = ft[c] + 1 if c in ft else 1
-    return ft
+    pass
 
 def htree_from_freqtable(ft: dict) -> Tree:
     """Build a Huffman tree from a frequency table."""
-    queue = PriorityQueue()
-    for (k,v) in ft.items():
-        queue.put((v, Tree((k,v), None, None)))
-    while queue.qsize() > 1:
-        (f1,n1) = queue.get()
-        (f2,n2) = queue.get()
-        queue.put((f1+f2, Tree(('',f1+f2), n1, n2)))
-    return queue.get()[1]
+    pass
 
 def build_code(t: Tree) -> dict:
     """Build a Huffman code from a Huffman tree."""
-    def make_path(t: Tree, path: list) -> dict:
-        """Construct a list of bools representing a path from the root to a leaf."""
-        if t.left is None and t.right is None:
-            return {t.value[0]: path}
-        else:
-            lh = make_path(t.left, path + [False]) if t.left else {}
-            rh = make_path(t.right, path + [True]) if t.right else {}
-            lh.update(rh)
-            return lh 
-    return make_path(t, [])
+    pass
 
 def encode(input: str) -> tuple:
     """Make the Huffman coding of the input string to a tuple of Huffman code 
        and encoded data.
     """
-    ft = freq_table(input)
-    ht = htree_from_freqtable(ft)
-    code = build_code(ht)
-    data = []
-    for c in input:
-        data.extend(code[c])
-    return (code, data)
+    pass
 
 def tree_from_code(code: dict) -> Tree:
     """Reconstruct the Huffman tree from a Huffman code."""
-    t = Tree(None, None, None)
-    node = t
-    for (k,path) in code.items():
-        for i in range(len(path)):
-            leaf = i == len(path)-1
-            next = Tree((k,0), None, None) if leaf else Tree(None, None, None)
-            if path[i]:
-                if node.right is None:
-                    node.right = next
-                node = node.right
-            else:
-                if node.left is None:
-                    node.left = next
-                node = node.left
-        node = t
-    return t
+    pass
 
 def decode(code: dict, data: list) -> str:
     """Decode the data using the Huffman code."""
-    t = tree_from_code(code)
-    node = t
-    secret = ""
-    for b in data:
-        if node.is_leaf():
-            node = t
-        node = node.right if b else node.left
-        if node.is_leaf():
-            secret += node.value[0]
-    return secret
-
-#def print(t: Tree):
-#    pt = PrettyPrintTree(lambda x: x.children(), lambda x: x.value)
-#    pt(t)
+    pass
